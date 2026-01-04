dd7167f65cefc991e62cd1539424851da4bb4585
Pramudithalakshan <pramudithalakshan00@gmail.com>
2026-01-04 18:41:30 +0530

    Fix : Configure environment variables and MongoDB connection

--- src/main/java/org/craftigen/Main.java
+++ src/main/java/org/craftigen/Main.java
@@ -1,5 +1,6 @@
 package org.craftigen;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,5 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
-
     }
 }
